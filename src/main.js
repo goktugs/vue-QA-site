12377799ebfce6474e6b114e62a353bd8223faa9
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-05-16 20:17:53 +0300

    Add axios globally

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import '@/assets/css/app.css'; 
+import '@/assets/css/app.css';
+import appAxios from '@/axios/appAxios';
 
-createApp(App).use(store).use(router).mount('#app');
+// global olarak zıng diye erişmek için
+app.config.globalProperties.$appAxios = appAxios;
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount('#app');
