b48b68b44628e0bb42621e0754bd476e7cf629a2
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-07-19 12:07:06 +0300

    Add another Mixins and small changes

--- src/components/AppShared/AppAlert.vue
+++ src/components/AppShared/AppAlert.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="row mt-5">
+  <div class="row">
     <div class="col-12">
       <div class="alert alert-primary" role="alert">
-        Bu Kategori(lere) ait bir soru bulunamadı. Soru sormak için
-        <a href="#">tıklayınız</a>
+        <slot></slot>
       </div>
     </div>
   </div>
--- src/mixins/helperMixins.js
+++ src/mixins/helperMixins.js
@@ -1,7 +1,32 @@
+import moment from 'moment';
+
+moment.updateLocale('en', {
+  relativeTime: {
+    future: '%s sonra',
+    past: '%s önce',
+    s: 'birkaç saniye',
+    m: '1 dakika',
+    mm: '%d dakika',
+    h: '1 saat',
+    hh: '%d saat',
+    d: '1 gün',
+    dd: '%d gün',
+    M: '1 ay',
+    MM: '%d ay',
+    y: '1 yıl',
+    yy: '%d yıl',
+  },
+});
 export default {
   methods: {
     copy(myObject) {
       return JSON.parse(JSON.stringify(myObject));
+    },
+    timesAgo(time) {
+      return moment(time).fromNow();
+    },
+    _createdAtInfo(fullName, createdAt, postFix) {
+      return `${fullName} ${this.timesAgo(createdAt)} ${postFix || 'sordu'}`;
     },
   },
 };
