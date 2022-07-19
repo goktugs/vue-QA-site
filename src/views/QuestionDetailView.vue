4e64aab13f2c42b6d94e9ebae998c554fe4cedf0
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-07-19 10:48:28 +0300

    Add Question Detail Page

--- src/views/QuestionDetailView.vue
+++ src/views/QuestionDetailView.vue
@@ -1,107 +1,120 @@
+<script>
+import {mapGetters} from "vuex";
+import CommentSection from "@/components/QuestionDetailView/CommentSection"
+
+export default {
+  components: {CommentSection},
+  data() {
+    return {
+      question: null,
+      isLoaded: false,
+      userData: {
+        question: {
+          details: null,
+        },
+        comment: {
+          details: null,
+        }
+      },
+      editMode: false,
+      provideData: {
+        commentList: []
+      }
+    }
+  },
+  provide() {
+    return {
+      onSaveComment: this.onSaveComment,
+      commentList: this.provideData.commentList
+    }
+  },
+  methods: {
+    fetchQuestion() {
+      return this.$appAxios.get(`/questions?id=${this.$route.params.id}&_expand=category&_expand=user`).then(({data: questionData}) => {
+        this.question = questionData[0] || {}
+        this.userData.question.details = this.question.details;
+      })
+    },
+    updateQuestion() {
+      this.$appAxios.patch(`/questions/${this.$route.params.id}`, this.userData.question).then(({data: questionData}) => {
+        this.question.details = questionData.details;
+        this.editMode = false;
+      })
+    },
+    onSaveComment(details) {
+      const userData = {
+        details,
+        userId: this.currentUser.id,
+        questionId: this.question.id,
+        created_at: new Date()
+      }
+      this.$appAxios.post("/comments", userData).then(({ data : comment }) => {
+        this.provideData.commentList.push(comment);
+      })
+    },
+    fetchComments() {
+      this.$appAxios.get(`/comments?questionId=${this.question.id}&_expand=user`).then(({data: commentList}) => {
+        this.provideData.commentList.push(...commentList)
+        this.isLoaded = true;
+      })
+    }
+  },
+  mounted() {
+    if (this.$route.params.id) {
+      this.fetchQuestion().then(() => {
+        this.fetchComments()
+      })
+    }
+  },
+  computed: {
+    ...mapGetters({
+      currentUser: "users/currentUser"
+    }),
+    isUserQuestionOwner() {
+      return this.currentUser?.id === this.question?.userId
+    },
+    createdAtInfo() {
+      return this._createdAtInfo(this.question.user.full_name, this.question.created_at)
+    }
+  }
+}
+</script>
 <template>
-  <div class="container question-detail-page">
+  <div v-if="isLoaded" class="container question-detail-page">
     <div class="row mt-3">
       <div class="col-10 offset-1">
         <div class="card text-left mb-3">
           <div class="card-body">
-            <div
-              class="d-flex justify-content-between align-items-center w-100"
-            >
-              <h3 class="card-title">Soru Başlığı</h3>
-              <span>
-                <a href="#" class="btn btn-link text-primary pe-0">
-                  <i class="fa fa-edit"></i> Düzenle
-                </a>
-              </span>
+            <div class="d-flex justify-content-between align-items-center w-100">
+              <h3 class="card-title">{{ question?.title || "-" }}</h3>
+              <span v-if="isUserQuestionOwner && !editMode">
+                  <a @click="editMode = true" class="btn btn-link text-primary pe-0">
+                    <i class="fa fa-edit"></i> Düzenle
+                  </a>
+                </span>
             </div>
             <div class="mt-3">
-              <div class="form-group mb-3">
-                Editör Burada olacak..
-                <div
-                  class="
-                    button-action-container
-                    mt-2
-                    d-flex
-                    justify-content-end
-                    align-items-center
-                  "
-                >
-                  <button class="btn btn-sm btn-secondary me-1">İptal</button>
-                  <button class="btn btn-sm btn-primary">Düzenle</button>
+              <div v-if="editMode" class="form-group mb-3">
+                <quill-editor v-model:value="userData.question.details"/>
+                <div class="button-action-container mt-2 d-flex justify-content-end align-items-center">
+                  <button @click="editMode = false" class="btn btn-sm btn-secondary me-1">İptal</button>
+                  <button @click="updateQuestion" class="btn btn-sm btn-primary">Düzenle</button>
                 </div>
               </div>
-
-              <p class="text-muted">Soru Detayları...</p>
+              <p v-else class="text-muted" v-html="question.details"></p>
 
               <div class="d-flex justify-content-between align-items-center">
                 <div class="custom-text-light">
                   <i class="fa fa-user-circle me-1"></i>
-                  Gökhan 1 ay önce cevapladı.
+                  {{ createdAtInfo }}
                 </div>
-                <div class="custom-text-light">Kategori Adı</div>
-              </div>
-            </div>
-          </div>
-          <div
-            class="
-              card-footer
-              text-muted
-              py-3
-              question-footer
-              flex-column
-              d-flex
-              justify-content-between
-              align-items-center
-              w-100
-            "
-          >
-            <!-- CEVAPLAR-->
-            <!-- Cevap Yaz Bölümü -->
-            <div class="answerArea w-100 mb-3">
-              <div class="form-group w-100 mb-1">Editör Alanı</div>
-              <div class="d-flex justify-content-end align-items-center">
-                <button class="btn btn-primary btn-sm">Gönder</button>
-              </div>
-            </div>
-            <!-- /Cevap Yaz Bölümü -->
-
-            <div class="card text-left mb-3 ms-1 w-100">
-              <div class="card-body">
-                <div class="mt-3">
-                  <p class="text-muted">Comment Detayları</p>
-                </div>
-              </div>
-              <div
-                class="
-                  card-footer
-                  text-muted
-                  py-2
-                  question-footer
-                  d-flex
-                  justify-content-between
-                  align-items-center
-                "
-              >
                 <div class="custom-text-light">
-                  <i class="fa fa-user-circle me-1"></i>
-                  1 ay önce cevapladı
-                </div>
-                <div class="question-action-container">
-                  <a href="#" class="like-btn text-muted me-2"
-                    ><i class="fa fa-thumbs-up text-success"></i>(4)</a
-                  >
-                  <a href="#" class="dislike-btn text-muted">
-                    <i class="fa fa-thumbs-down"></i> (4)
-                  </a>
+                  {{ question.category.title }}
                 </div>
               </div>
             </div>
-
-            <div class="alert alert-primary w-100" role="alert">
-              Bu Soruya ait bir cevap bulunamadı.
-            </div>
-            <!-- !CEVAPLAR-->
           </div>
+          <CommentSection/>
         </div>
       </div>
     </div>
