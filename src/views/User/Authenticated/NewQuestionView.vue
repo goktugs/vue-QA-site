11ab0bf721ab1418f2ef161afcf446c074285096
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-07-19 12:04:33 +0300

    Add Date Functionality

--- src/views/User/Authenticated/NewQuestionView.vue
+++ src/views/User/Authenticated/NewQuestionView.vue
@@ -1,89 +1,63 @@
 <script>
-import { mapGetters } from "vuex";
+import {mapGetters} from "vuex";
 export default {
   data: () => ({
     userData: {
-      details: null,
-      title: null,
-      categoryId: null,
-    },
+      title : null,
+      details : null,
+      categoryId : null,
+    }
   }),
-  methods: {
-    onSubmit() {
-      const userData = this.copy(this.userData);
-      userData.userId = this.currentUser.id;
+  methods : {
+    onSubmit(){
+      const userData = {
+        ...this.copy(this.userData),
+        created_at : new Date(),
+        userId : this.currentUser.id
+      }
       this.$appAxios.post("/questions", userData).then(({ status }) => {
-        if (status === 201) {
-          this.$router.push({ name: "HomeView" });
-        }
-      });
-    },
+        if(status === 201) this.$router.push({ name : 'HomeView'})
+      })
+    }
   },
   computed: {
     ...mapGetters({
       categoryList: "categories/categoryList",
-      currentUser: "users/currentUser",
-    }),
-  },
-};
+      currentUser : "users/currentUser"
+    })
+  }
+}
 </script>
-
-
-
 <template>
   <div class="container">
     <div class="row mt-3">
       <div class="col-8 offset-2">
         <div class="card">
-          <div class="card-header">Yeni Soru</div>
+          <div class="card-header">
+            Yeni Soru
+          </div>
           <div class="card-body">
             <div class="mb-3">
               <label for="question-title" class="form-label"
-                >Kısa bir başlık</label
+              >Kısa bir başlık</label
               >
-              <input
-                v-model="userData.title"
-                type="text"
-                class="form-control"
-                id="question-title"
-                placeholder="Başlık"
-              />
+              <input v-model="userData.title" type="text" class="form-control" id="question-title" placeholder="Başlık"/>
             </div>
             <div class="mb-3">
               <label class="form-label">Detaylar</label>
-              <quill-editor v-model:value="userData.content" />
+              <quill-editor v-model:value="userData.details"/>
             </div>
             <div class="mb-3">
               <label for="question-category" class="form-label">Kategori</label>
-              <select
-                class="form-select"
-                v-model="userData.categoryId"
-                id="question-category"
-              >
+              <select v-model="userData.categoryId" class="form-select" id="question-category">
                 <option selected>Lütfen bir kategori seçiniz</option>
-                <option
-                  v-for="category in categoryList"
-                  :key="category.id"
-                  :value="category.id"
-                >
-                  {{ category.title }}
-                </option>
+                <option v-for="category in categoryList" :value="category.id" :key="category.id">{{category.title }}</option>
               </select>
             </div>
           </div>
 
-          <div
-            class="
-              card-footer
-              text-muted
-              d-flex
-              justify-content-end
-              align-items-center
-            "
-          >
-            <button @click="onSubmit" class="btn btn-sm btn-primary">
-              Kaydet
-            </button>
+          <div class="card-footer text-muted d-flex justify-content-end align-items-center">
+            <button @click="onSubmit" class="btn btn-sm btn-primary">Kaydet</button>
           </div>
         </div>
       </div>
